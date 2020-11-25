3ba4fc2cf568712b841244fac96e8b725cb9b523
Alex <alexbibig@gmail.com>
2020-11-25 13:35:53 +0300

    create some minor changes in style and functionality

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,118 +1,33 @@
-let numbersOfButtons = 16;
-let oneButtonSize = 75;
-let buttonSizes = 74;
-let moveCounter = 0;
-let sound = true;
-let gamePaused = false;
-let gameTime = 0;
-
-let positions = [];
+let numbersOfButtons = 16,
+  oneButtonSize = 75,
+  buttonSizes = 74,
+  moveCounter = 0,
+  sound = true,
+  gamePaused = false,
+  gameTime = 0,
+  positions = [];
 
 initElements();
 
-const grid = document.querySelector('.grid');
-const gridButton = document.querySelector('.grid-button');
-const move = document.querySelector('.move');
-const restartButton = document.querySelector('.button_restart');
-const pauseButton = document.querySelector('.button_pause');
-const saveGameButton = document.querySelector('.button_save');
-const timeCounter = document.querySelector('.time');
-
-const button3x3 = document.querySelector('.button3x3');
-const button4x4 = document.querySelector('.button4x4');
-const button5x5 = document.querySelector('.button5x5');
-const button6x6 = document.querySelector('.button6x6');
-const button7x7 = document.querySelector('.button7x7');
-const button8x8 = document.querySelector('.button8x8');
-
-const overlay = document.querySelector('.overlay');
-const resumeItems = document.querySelector('.resume-items');
-const soundButton = document.querySelector('.sound-button');
-const keepPlaying = document.querySelector('.keep-playing');
-const loadSavedGame = document.querySelector('.load-game');
-const newGame = document.querySelector('.new-game');
+const grid = document.querySelector('.grid'),
+  gridButton = document.querySelector('.grid-button'),
+  move = document.querySelector('.move'),
+  pauseButton = document.querySelector('.button_pause'),
+  saveGameButton = document.querySelector('.button_save'),
+  timeCounter = document.querySelector('.time'),
+  overlay = document.querySelector('.overlay'),
+  resumeItems = document.querySelector('.resume-items'),
+  soundButton = document.querySelector('.sound-button'),
+  keepPlaying = document.querySelector('.keep-playing'),
+  loadSavedGame = document.querySelector('.load-game'),
+  newGame = document.querySelector('.new-game'),
+  newGameItems = document.querySelector('.new-game-items'),
+  button3x3 = document.querySelector('.button3x3'),
+  button4x4 = document.querySelector('.button4x4'),
+  button5x5 = document.querySelector('.button5x5'),
+  button6x6 = document.querySelector('.button6x6');
 
 function initElements() {
-  //init game
-
-  const game = document.createElement('div');
-  game.classList.add('game');
-
-  const menu = document.createElement('div');
-  menu.classList.add('menu');
-
-  const buttonPause = document.createElement('button');
-  buttonPause.classList.add('button_pause');
-  buttonPause.innerHTML = 'Pause';
-  menu.append(buttonPause);
-
-  const buttonRestart = document.createElement('button');
-  buttonRestart.classList.add('button_restart');
-  buttonRestart.innerHTML = 'New game';
-  menu.append(buttonRestart);
-
-  const buttonSave = document.createElement('button');
-  buttonSave.classList.add('button_save');
-  buttonSave.innerHTML = 'Save game';
-  menu.append(buttonSave);
-
-  const grid = document.createElement('div');
-  grid.classList.add('grid');
-
-  const stats = document.createElement('div');
-  stats.classList.add('stats');
-
-  const move = document.createElement('span');
-  move.classList.add('move');
-  stats.append(move);
-
-  const timeCounter = document.createElement('span');
-  timeCounter.classList.add('time');
-  stats.append(timeCounter);
-
-  // field selector
-
-  const fieldSelector = document.createElement('div');
-  fieldSelector.classList.add('menu', 'field-selector');
-
-  const button3x3 = document.createElement('button');
-  button3x3.classList.add('button3x3');
-  button3x3.innerHTML = '3 x 3';
-  fieldSelector.append(button3x3);
-
-  const button4x4 = document.createElement('button');
-  button4x4.classList.add('button4x4');
-  button4x4.innerHTML = '4 x 4';
-  fieldSelector.append(button4x4);
-
-  const button5x5 = document.createElement('button');
-  button5x5.classList.add('button5x5');
-  button5x5.innerHTML = '5 x 5';
-  fieldSelector.append(button5x5);
-
-  const button6x6 = document.createElement('button');
-  button6x6.classList.add('button6x6');
-  button6x6.innerHTML = '6 x 6';
-  fieldSelector.append(button6x6);
-
-  const button7x7 = document.createElement('button');
-  button7x7.classList.add('button7x7');
-  button7x7.innerHTML = '7 x 7';
-  fieldSelector.append(button7x7);
-
-  const button8x8 = document.createElement('button');
-  button8x8.classList.add('button8x8');
-  button8x8.innerHTML = '8 x 8';
-  fieldSelector.append(button8x8);
-
-  game.append(menu);
-  game.append(grid);
-  game.append(stats);
-  game.append(fieldSelector);
-  document.body.append(game);
-
-  // init overlay
-
   const overlay = document.createElement('div');
   overlay.classList.add('overlay');
 
@@ -139,7 +54,34 @@ function initElements() {
   soundButton.innerHTML = `<i class="material-icons">volume_up</i>`;
   resumeItems.append(soundButton);
 
+  const newGameItems = document.createElement('div');
+  newGameItems.classList.add('new-game-items');
+
+  const button3x3 = document.createElement('button');
+  button3x3.classList.add('button3x3');
+  button3x3.innerHTML = '3 x 3';
+  newGameItems.append(button3x3);
+
+  const button4x4 = document.createElement('button');
+  button4x4.classList.add('button4x4');
+  button4x4.innerHTML = '4 x 4';
+  newGameItems.append(button4x4);
+
+  const button5x5 = document.createElement('button');
+  button5x5.classList.add('button5x5');
+  button5x5.innerHTML = '5 x 5';
+  newGameItems.append(button5x5);
+
+  const button6x6 = document.createElement('button');
+  button6x6.classList.add('button6x6');
+  button6x6.innerHTML = '6 x 6';
+  newGameItems.append(button6x6);
+
   overlay.append(resumeItems);
+  overlay.append(newGameItems);
+
+  newGameItems.classList.add('hidden');
+
   document.body.append(overlay);
 }
 
@@ -184,9 +126,17 @@ function initGame(savedGameValues) {
   }
 }
 
-function loadGame(savedGameValues, savedGameMoves, savedGameTime) {
+function loadGame(
+  savedGameValues,
+  savedGameMoves,
+  savedGameTime,
+  savedOneButtonSize,
+  savedButtonSizes,
+) {
   moveCounter = savedGameMoves;
   gameTime = savedGameTime;
+  oneButtonSize = Number(savedOneButtonSize);
+  buttonSizes = Number(savedButtonSizes);
 
   grid.innerHTML = '';
   positions = [];
@@ -207,8 +157,6 @@ function randomize() {
     for (rsh = j = 0; j < 14; rsh += s, j++)
       for (s = 0, k = j + 1; k < 15; k++) if (rnd[k] < rnd[j]) s++;
   }
-
-  // rnd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 0];
 
   return rnd;
 }
@@ -305,10 +253,6 @@ function showTime() {
   timeCounter.innerHTML = `Game time: ${minutes}:${seconds}`;
 }
 
-restartButton.addEventListener('click', () => {
-  restart();
-});
-
 pauseButton.addEventListener('click', () => {
   overlay.classList.add('overlay_show');
   gamePaused = !gamePaused;
@@ -319,6 +263,8 @@ saveGameButton.addEventListener('click', () => {
   localStorage.setItem('positions', JSON.stringify(positions));
   localStorage.setItem('moves', moveCounter);
   localStorage.setItem('time', gameTime);
+  localStorage.setItem('oneButtonSize', oneButtonSize);
+  localStorage.setItem('buttonSizes', buttonSizes);
 });
 
 keepPlaying.addEventListener('click', () => {
@@ -334,8 +280,10 @@ loadSavedGame.addEventListener('click', () => {
   const savedGameValues = JSON.parse(localStorage.getItem('positions'));
   const savedGameMoves = localStorage.getItem('moves');
   const savedGameTime = localStorage.getItem('time');
+  const savedOneButtonSize = localStorage.getItem('oneButtonSize', oneButtonSize);
+  const savedButtonSizes = localStorage.getItem('buttonSizes', buttonSizes);
 
-  loadGame(savedGameValues, savedGameMoves, savedGameTime);
+  loadGame(savedGameValues, savedGameMoves, savedGameTime, savedOneButtonSize, savedButtonSizes);
   overlay.classList.remove('overlay_show');
 
   gamePaused = !gamePaused;
@@ -347,9 +295,8 @@ loadSavedGame.addEventListener('click', () => {
 });
 
 newGame.addEventListener('click', () => {
-  overlay.classList.remove('overlay_show');
-  restart();
-  gamePaused = !gamePaused;
+  resumeItems.classList.add('hidden');
+  newGameItems.classList.remove('hidden');
 
   if (keepPlaying.attributes.disabled) {
     keepPlaying.removeAttribute('disabled');
@@ -369,7 +316,11 @@ button3x3.addEventListener('click', () => {
   oneButtonSize = 100;
   buttonSizes = 99;
 
+  overlay.classList.remove('overlay_show');
+  resumeItems.classList.remove('hidden');
+  newGameItems.classList.add('hidden');
   restart();
+  gamePaused = !gamePaused;
 });
 
 button4x4.addEventListener('click', () => {
@@ -377,7 +328,11 @@ button4x4.addEventListener('click', () => {
   oneButtonSize = 75;
   buttonSizes = 74;
 
+  overlay.classList.remove('overlay_show');
+  resumeItems.classList.remove('hidden');
+  newGameItems.classList.add('hidden');
   restart();
+  gamePaused = !gamePaused;
 });
 
 button5x5.addEventListener('click', () => {
@@ -385,7 +340,11 @@ button5x5.addEventListener('click', () => {
   oneButtonSize = 60;
   buttonSizes = 59;
 
+  overlay.classList.remove('overlay_show');
+  resumeItems.classList.remove('hidden');
+  newGameItems.classList.add('hidden');
   restart();
+  gamePaused = !gamePaused;
 });
 
 button6x6.addEventListener('click', () => {
@@ -393,23 +352,11 @@ button6x6.addEventListener('click', () => {
   oneButtonSize = 50;
   buttonSizes = 49;
 
+  overlay.classList.remove('overlay_show');
+  resumeItems.classList.remove('hidden');
+  newGameItems.classList.add('hidden');
   restart();
-});
-
-button7x7.addEventListener('click', () => {
-  numbersOfButtons = 49;
-  oneButtonSize = 43;
-  buttonSizes = 42;
-
-  restart();
-});
-
-button8x8.addEventListener('click', () => {
-  numbersOfButtons = 64;
-  oneButtonSize = 38;
-  buttonSizes = 36;
-
-  restart();
+  gamePaused = !gamePaused;
 });
 
 initGame();
